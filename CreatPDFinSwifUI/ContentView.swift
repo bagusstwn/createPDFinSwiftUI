dc8b416f575b876d935ba241fa815fbd0d451c73
Bagus setiawan <setiawanbagus4315@gmail.com>
2022-01-12 09:00:28 +0700

    add files

---
+++ CreatPDFinSwifUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CreatPDFinSwifUI
+//
+//  Created by Bagus setiawan on 12/01/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CreatPDFinSwifUI/CreatPDFinSwifUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CreatPDFinSwifUIApp.swift
+//  CreatPDFinSwifUI
+//
+//  Created by Bagus setiawan on 12/01/22.
+//
+
+import SwiftUI
+
+@main
+struct CreatPDFinSwifUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
